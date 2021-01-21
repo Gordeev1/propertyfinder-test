c753134ffe4d4c2d37120fb1ff29ae14352bc21d
Artem Gordeev <gordeevartem.dev@gmail.com>
2021-01-21 03:11:46 +0300

    fix(SegmentedControl): use default colors for android

--- src/components/SegmentedControl/styled.ts
+++ src/components/SegmentedControl/styled.ts
@@ -1,11 +1,12 @@
 import SegmentedControl from '@react-native-community/segmented-control';
 import styled from '@utils/styled-components';
+import { isIOS } from '@constants';
 
 export const ConfiguredSegmentedControlBase = styled(SegmentedControl).attrs((p) => ({
-	tintColor: p.theme.colors.segmentedControlTintColor,
+	tintColor: isIOS ? p.theme.colors.segmentedControlTintColor : undefined,
 	fontStyle: {
 		fontFamily: p.theme.colors.fontMain,
-		color: p.theme.colors.segmentedControlTextColor,
+		color: isIOS ? p.theme.colors.segmentedControlTextColor : undefined,
 	},
 }))`
 	flex: 2;
