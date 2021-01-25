87e081322cfa6f17f19870d90c68281a35ccb82d
Artem Gordeev <gordeevartem.dev@gmail.com>
2021-01-25 19:14:38 +0300

    feat(settings): add horizontal layout

--- src/scenes/Settings/styled.ts
+++ src/scenes/Settings/styled.ts
@@ -1,7 +1,11 @@
-import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import styled from '@utils/styled-components';
-import { scale } from '@utils/styles';
+import { scale, scaleVertical } from '@utils/styles';
 
-export const Container = styled(View)`
-	padding-horizontal: ${scale(15)};
-`;
+export const Container = styled(ScrollView).attrs((p) => ({
+	contentContainerStyle: {
+		paddingLeft: p.theme.insets.left + scale(15)(p),
+		paddingRight: p.theme.insets.right + scale(15)(p),
+		paddingBottom: scaleVertical(15)(p),
+	},
+}))``;
